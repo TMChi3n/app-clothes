6edf52eee27f7c0c3046305024dcce9e3ba6cf33
TMChi3n <tranminhchien15052003@gmail.com>
2024-06-15 15:07:09 +0700

    fix: update phone_number entity user and database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 8080;
   await app.listen(port);
