e94a9e54cce305a8f100a1eb8c182346628990d0
hieunt1103 <hieunt1103@gmail.com>
2024-06-30 21:12:15 +0700

    update product

--- backend/src/modules/product/dto/update-product.dto.ts
+++ backend/src/modules/product/dto/update-product.dto.ts
@@ -1,11 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
-import { IsString, IsOptional, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsString, IsOptional, IsNumber } from 'class-validator';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
     @IsOptional()
     @IsString()
-    @IsNotEmpty()
     name?: string;
   
     @IsOptional()
@@ -22,16 +21,13 @@ export class UpdateProductDto extends PartialType(CreateProductDto) {
   
     @IsOptional()
     @IsNumber()
-    @IsNotEmpty()
     price?: number;
   
     @IsOptional()
     @IsString()
-    @IsNotEmpty()
     img_url?: string;
   
     @IsOptional()
     @IsString()
-    @IsNotEmpty()
     type?: string;
   }
