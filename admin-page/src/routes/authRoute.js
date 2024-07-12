c5f6225706d18b32f7ab9a5ec8125e8d4ec986de
TMChi3n <tranminhchien15052003@gmail.com>
2024-07-12 09:48:20 +0700

    remove admin

--- admin-page/src/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const AuthRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem("access_token");
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default AuthRoute;
--- admin-page/src/utils/baseUrl.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const url = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
-});
-
-export default url;
